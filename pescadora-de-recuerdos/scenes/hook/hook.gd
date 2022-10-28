b27352d49b71083da21e81b48434f3c21b6a4229
PuntitOwO <christopher.marin@ug.uchile.cl>
2022-10-28 16:25:42 -0300

    test scene and hook values

--- pescadora-de-recuerdos/scenes/hook/hook.gd
+++ pescadora-de-recuerdos/scenes/hook/hook.gd
@@ -1,23 +1,29 @@
 extends KinematicBody2D
 
-export var down_velocity = 10.0
+export var max_down_velocity = 10.0
+export var max_up_velocity = -100.0
+export var max_move_velocity = 300.0
 export var down_acceleration = 10
+export var up_decceleration = 10
 export var fast_down_velocity = 100
-export var up_velocity = -100.0
 export var move_velocity = 100
 
 var velocity := Vector2.ZERO
 
 func _physics_process(_delta: float) -> void:
-	velocity.y += down_acceleration
-	velocity.x -= down_acceleration * sign(velocity.x)
-	velocity.y = clamp(velocity.y, up_velocity, down_velocity)
-	if Input.is_action_pressed("move_down"):
+	if velocity.y < 0:
+		velocity.y += up_decceleration
+	else:
+		velocity.y += down_acceleration
+	velocity.x -= up_decceleration * sign(velocity.x)
+	velocity.y = clamp(velocity.y, max_up_velocity, max_down_velocity)
+	if Input.is_action_pressed("move_down") and velocity.y >= 0.0:
 		velocity.y = fast_down_velocity
 	if Input.is_action_just_pressed("move_left"):
-		velocity.y = up_velocity
+		velocity.y = max_up_velocity
 		velocity.x -= move_velocity
 	if Input.is_action_just_pressed("move_right"):
-		velocity.y = up_velocity
+		velocity.y = max_up_velocity
 		velocity.x += move_velocity
+	velocity.x = clamp(velocity.x, -max_move_velocity, max_move_velocity)
 	velocity = move_and_slide(velocity, Vector2.UP)
