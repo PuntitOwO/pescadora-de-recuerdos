fbfeee28eb04d43ef7e52047fa1e92d06bff89f9
Jorge Valenzuela <jorgevalenzuela@ug.uchile.cl>
2022-10-28 17:27:17 -0300

    enemigo que sigue al anzuelo

---
+++ pescadora-de-recuerdos/scenes/enemies/follow_enemy/follow_enemy.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var speed = 3.0
+export (NodePath) var node_attached_path
+var node_attached 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	node_attached = get_node(node_attached_path)
+	
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var init_pos = get_position()
+	var player_pos = node_attached.global_position
+	var direction = (player_pos - init_pos) / sqrt(pow(player_pos[0]-init_pos[0],2)+pow(player_pos[1]-init_pos[1],2))
+	position.x +=  direction[0]*speed
+	position.y += direction[1]*speed
+#	pass
--- pescadora-de-recuerdos/scenes/fishing_line/fishing_line.gd
+++ pescadora-de-recuerdos/scenes/fishing_line/fishing_line.gd
@@ -4,16 +4,21 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-export (NodePath) var node_attached_path
-var node_attached 
+export (NodePath) var end_node_attached_path
+var end_node_attached 
+export (NodePath) var start_node_attached_path
+var start_node_attached 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	node_attached = get_node(node_attached_path)
+	start_node_attached = get_node(start_node_attached_path)
+	end_node_attached = get_node(end_node_attached_path)
 	$Line2D.set_as_toplevel(true)
-	$Line2D.set_point_position(0,Vector2(0,-200))
-	$Line2D.set_point_position(1,node_attached.global_position)
+	$Line2D.set_point_position(0,start_node_attached.global_position)
+	$Line2D.set_point_position(1,end_node_attached.global_position)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	$Line2D.set_point_position(1,node_attached.global_position)
+	$Line2D.set_point_position(0,start_node_attached.global_position)
+	$Line2D.set_point_position(1,end_node_attached.global_position)
+	
 #	pass
