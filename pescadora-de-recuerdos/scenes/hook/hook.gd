f7dc31004041167079f8840134bd31b07294e76f
Jorge Valenzuela <jorgevalenzuela@ug.uchile.cl>
2022-10-29 12:42:06 -0300

    particulas hook

--- pescadora-de-recuerdos/scenes/hook/hook.gd
+++ pescadora-de-recuerdos/scenes/hook/hook.gd
@@ -8,12 +8,15 @@ export var up_decceleration = 10
 export var fast_down_velocity = 150
 export var move_velocity = 200
 export var knockback_strength = 150
-
+export var bubble_duration = 60
+var bubble_time_activated = 0
 var velocity := Vector2.ZERO
 
 func _ready():
 	get_viewport().audio_listener_enable_2d = true
-
+	$bubbles1.emitting = false
+	$bubbles2.emitting = false
+	
 func _physics_process(_delta: float) -> void:
 	if velocity.y < 0:
 		velocity.y += up_decceleration
@@ -22,15 +25,30 @@ func _physics_process(_delta: float) -> void:
 	velocity.x -= up_decceleration * sign(velocity.x)
 	velocity.y = clamp(velocity.y, max_up_velocity, max_down_velocity)
 	if Input.is_action_pressed("move_down") and velocity.y >= 0.0:
+		$bubbles1.emitting = true
+		$bubbles2.emitting = true
 		velocity.y = fast_down_velocity
+	if Input.is_action_just_released("move_down"):
+		$bubbles1.emitting = false
+		$bubbles2.emitting = false
 	if Input.is_action_just_pressed("move_left"):
+		if bubble_time_activated <= bubble_duration:
+			$bubbles1.emitting = true
+			$bubbles2.emitting = true
 		$hook_pull.play()
 		velocity.y = max_up_velocity
 		velocity.x -= move_velocity
 	if Input.is_action_just_pressed("move_right"):
+		if bubble_time_activated <= bubble_duration:
+			$bubbles1.emitting = true
 		$hook_pull.play()
 		velocity.y = max_up_velocity
 		velocity.x += move_velocity
+	bubble_time_activated+=1
+	if bubble_time_activated >= bubble_duration:
+		$bubbles1.emitting = false
+		$bubbles2.emitting = false
+		bubble_time_activated = 0
 	velocity.x = clamp(velocity.x, -max_move_velocity, max_move_velocity)
 	velocity = move_and_slide(velocity, Vector2.UP)
 	#collisions
