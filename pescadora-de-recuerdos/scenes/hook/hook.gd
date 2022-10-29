a5f51788a25ab91d4794cdf7a18f3958c5bfef80
PuntitOwO <christopher.marin@ug.uchile.cl>
2022-10-29 01:47:29 -0300

    hook knockback

--- pescadora-de-recuerdos/scenes/hook/hook.gd
+++ pescadora-de-recuerdos/scenes/hook/hook.gd
@@ -1,17 +1,15 @@
 extends KinematicBody2D
 
-export var max_down_velocity = 30.0
+export var max_down_velocity = 150.0
 export var max_up_velocity = -300.0
 export var max_move_velocity = 300.0
 export var down_acceleration = 10
 export var up_decceleration = 10
 export var fast_down_velocity = 150
 export var move_velocity = 200
+export var knockback_strength = 150
 
 var velocity := Vector2.ZERO
-
-func _ready() -> void:
-	$Area2D.connect("body_entered", self, "_on_body_entered")
 
 func _physics_process(_delta: float) -> void:
 	if velocity.y < 0:
@@ -30,9 +28,16 @@ func _physics_process(_delta: float) -> void:
 		velocity.x += move_velocity
 	velocity.x = clamp(velocity.x, -max_move_velocity, max_move_velocity)
 	velocity = move_and_slide(velocity, Vector2.UP)
+	#collisions
+	for i in get_slide_count():
+		var collision := get_slide_collision(i)
+		if collision.collider is VoidTile:
+			var knockback_direction = collision.position.direction_to(global_position)
+			_knockback(knockback_direction)
 
-func _on_body_entered(body:Node):
-	if body is VoidTile:
-		# take damage somehow
-		print("ouch")
-		pass
+func _knockback(direction:Vector2) -> void:
+	set_physics_process(false)
+	velocity = Vector2.ZERO
+	var tween = create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT).set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	tween.tween_method(self, "move_and_slide", direction * knockback_strength, Vector2.ZERO, 0.6)
+	tween.tween_callback(self, "set_physics_process", [true])
