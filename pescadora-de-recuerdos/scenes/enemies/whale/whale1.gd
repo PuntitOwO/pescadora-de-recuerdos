879012ce838fdb17459218da65620a8e0a270d52
Jorge Valenzuela <jorgevalenzuela@ug.uchile.cl>
2022-10-30 03:05:18 -0300

    whale1 collision disabled

--- pescadora-de-recuerdos/scenes/enemies/whale/whale1.gd
+++ pescadora-de-recuerdos/scenes/enemies/whale/whale1.gd
@@ -9,7 +9,7 @@ var player_near = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
+	$CollisionShape2D.disabled = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
