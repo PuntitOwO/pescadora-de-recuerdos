d1d23675af86c312c604d15f51f6f3b35482ae55
Jorge Valenzuela <jorgevalenzuela@ug.uchile.cl>
2022-10-30 03:37:43 -0300

    blobs nivel 3

--- pescadora-de-recuerdos/scenes/enemies/blob_3/blob3.gd
+++ pescadora-de-recuerdos/scenes/enemies/blob_3/blob3.gd
@@ -9,6 +9,7 @@ onready var b_dir_y = $Sprite.material.get_shader_param("b_dir_y")
 export var speed = 3.0
 export (NodePath) var node_attached_path
 var node_attached 
+var triggered = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -35,9 +36,12 @@ func on_TimerB_timeout():
 	$Sprite.material.set_shader_param("b_dir_y",b_dir_y)
 
 func _physics_process(delta):
-	var init_pos = get_position()
-	var player_pos = node_attached.global_position
-	var direction = (player_pos - init_pos) / sqrt(pow(player_pos[0]-init_pos[0],2)+pow(player_pos[1]-init_pos[1],2))
-	position.x +=  direction[0]*speed
-	position.y += direction[1]*speed
-
+	rotation_degrees += 5
+	if triggered == true:
+		var init_pos = get_position()
+		var player_pos = node_attached.global_position
+		var direction = (player_pos - init_pos) / sqrt(pow(player_pos[0]-init_pos[0],2)+pow(player_pos[1]-init_pos[1],2))
+		position.x +=  direction[0]*speed
+		position.y += direction[1]*speed
+	elif abs(get_tree().get_nodes_in_group("Hook")[0].global_position[1] - global_position[1]) <= 800:
+		triggered = true 
